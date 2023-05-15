6352cf418c04df654f5c343fe69b0b35f79a6ab4
shohanurrahman1 <shohanurrahmanshohan.cs@gmail.com>
2023-05-15 23:38:09 +0600

    Read Part Done

--- index.php
+++ index.php
@@ -8,7 +8,45 @@
           <div class="col-lg-12">
             <h2 class="text-center pb-5">Employee Contact Details</h2>
 
-            
+            <!-- Table Start -->
+            <table class="table table-striped table-hover table-bordered">
+              <thead class="table-dark">
+                <tr>
+                  <th scope="col">#Sl.</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Phone No.</th>
+                  <th scope="col">Address</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <?php  
+                    $sql = "SELECT * FROM employee ORDER BY name ASC";
+                    $read = mysqli_query($db, $sql);
+                    $i = 0;
+
+                    while($row = mysqli_fetch_assoc($read)){
+                      $id         = $row['id'];
+                      $name       = $row['name'];
+                      $email      = $row['email'];
+                      $phone      = $row['phone'];
+                      $address    = $row['address'];
+                      $i++;
+                      ?>
+                        <tr>
+                          <th scope="row"><?php echo $i; ?></th>
+                          <td><?php echo $name; ?></td>
+                          <td><?php echo $email; ?></td>
+                          <td><?php echo $phone; ?></td>
+                          <td><?php echo $address; ?></td>
+                        </tr>   
+                   <?php }
+                ?>
+                 
+              </tbody>
+            </table>
+            <!-- Table End -->
           </div>
         </div>
       </div>
